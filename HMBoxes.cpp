e4ebb27ae3fb9ca352a31208512ef9503603e7b2
Eugene Feingold <eugene.feingold@gmail.com>
2012-07-30 08:19:55 -0700

    matrix support

---
+++ HMBoxes.cpp
@@ -0,0 +1,49 @@
+#include "HMBoxes.h"
+#include "StripUtils.h"
+#include "Settings.h"
+
+#define BOXES_WIDTH 6
+
+
+static int pattern[6][BOXES_WIDTH] = {
+    {0, 1, 0, 1, 0, 0},
+    {1, 1, 1, 1, 1, 0},
+    {1, 1, 1, 1, 1, 0},
+    {0, 1, 1, 1, 0, 0},
+    {0, 0, 1, 0, 0, 0}    
+};
+
+
+HMBoxes::HMBoxes(LPD8806 *strip, Settings *settings) {
+  this->strip = strip;
+  this->settings = settings;
+};
+
+
+
+
+void HMBoxes::init() {
+  j = BOXES_WIDTH * 10;
+};
+
+
+void HMBoxes::loop() {
+  uint32_t c = StripUtils().getColor(settings->brightness, 127, 0, 0);
+  
+  for (int i = 0; i < strip->numPixels(); i++) {
+    int p = i + j / 10;
+    strip->setPixelColor(i, (pattern[i / 18][p % BOXES_WIDTH] == 1) ? c : 0);
+  }
+  strip->show();
+  
+  
+  j--;
+  if (j < 0) {
+    j = BOXES_WIDTH * 10;
+  }
+ 
+  
+  
+};
+
+
--- Hat2.ino
+++ Hat2.ino
@@ -6,15 +6,20 @@
 
 #include "HMRainbow.h"
 #include "HMSolid.h"
+#include "HMBoxes.h"
 
 
 
 #define NUM_LIGHTS 96
-#define NUM_MODES 2
+#define NUM_MODES 3
 
 //----------- PINS ----------- 
 int dataPin  = 2;    // Yellow wire on Adafruit Pixels
 int clockPin = 3;    // Green wire on Adafruit Pixels
+
+int button1Pin = 9;
+int led1Pin = 8;
+
 
 int brightnessPin = A0;
 int microphonePin = A5;
@@ -27,34 +32,62 @@ Settings settings = Settings();
 IHatMode *modes[NUM_MODES];
 
 
+int btn1State = 0;
+int mode = 0;
 
 
 
 
 
 void setup() {
+
+  pinMode(button1Pin, INPUT);
+  pinMode(led1Pin, OUTPUT);
+
   //init modes
   modes[0] = new HMRainbow(&strip, &settings);
   modes[1] = new HMSolid(&strip, &settings);
-  
-  
+  modes[2] = new HMBoxes(&strip, &settings);
+
+
   //init Settings
   //Settings().setBrightness(0.0);
 
   for (int i = 0; i < NUM_MODES; i++) {
     modes[i]->init();
   }
-  
-  
+
+
   strip.begin();
-  
+
   Serial.begin(57600);
 }
 
 
 void loop() {
+  int b = digitalRead(button1Pin);
+
+  if (btn1State == 0 && b == HIGH) {
+    btn1State = 1;
+    digitalWrite(led1Pin, HIGH);
+
+    mode++;
+    if (mode >= NUM_MODES) {
+      mode = 0;
+    }
+
+    modes[mode]->init(); 
+
+  } 
+  else if (btn1State == 1 && b == LOW) {
+    btn1State = 0;
+    digitalWrite(led1Pin, LOW);
+  }
+
   settings.brightness = analogRead(brightnessPin) / 1024.0;
-    
-  modes[1]->loop();
-  
+
+  modes[mode]->loop();
+
 }
+
+

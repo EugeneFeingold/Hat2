a510a566cdf52d427668b216efb9b564df890438
Eugene Feingold <eugene.feingold@gmail.com>
2012-08-06 19:59:54 -0700

    non interrupt-based audio

--- Hat2.ino
+++ Hat2.ino
@@ -246,154 +246,287 @@ void hmSolidLoop() {
 
 //============ ============  AUDIO ============ ============ 
 
-#define NUM_BUCKETS 18
-#define DECAY_UP 0.4          //more is faster
-#define DECAY_DOWN 0.01
-#define SCALE_DECAY 0.2    //more is faster
-#define COLOR_SHIFT_SPEED 1 //less is faster
-
 #define  IR_AUDIO  5 // ADC channel to capture
 
-volatile byte audioBufferPosition = 0;
+ #define N_WAVE          1024    /* full length of Sinewave[] */
+ #define LOG2_N_WAVE     10      /* log2(N_WAVE) */
 
-int16_t capture[FFT_N]; 		/* Wave capturing buffer */
-complex_t bfly_buff[FFT_N]; 		/* FFT buffer */
-uint16_t spectrum[FFT_N/2]; 		/* Spectrum output buffer */
+ #define FFT_SIZE       64 
+ #define log2FFT       6
+ #define N             (2 * FFT_SIZE)
+ #define log2N         (log2FFT + 1)
 
-float sBuffer[NUM_BUCKETS];
 
-float minima[NUM_BUCKETS];
-float maxima[NUM_BUCKETS];
+int       x[N], fx[N];
+ int      incomingByte; 
+ int kraccums = 0, zlaccums = 0, snaccums = 0;
+ int kraccumn = 0, zlaccumn = 0, snaccumn = 0;
 
-int iColor = 0;
-
+ int sdvig = 32768; //DC bias of the ADC, approxim +2.5V. (kompensaciya post. sostavlyauschei).
+ int minim = 0; 
+ int maxim = 4000; //32000; 
+ int vrem;
+ float kdmp = 0.95;  //Smoothing constant. 
+ float kary = 0.999;  //AGC time constant. 
+                      //AGC affects visual display only, no AGC on analog part of the system
 
 
 void hmAudioInit() {
+  Serial.println(ADMUX, HEX);
   
-  /*  REFS0 : VCC use as a ref, IR_AUDIO : channel selection, ADEN : ADC Enable, ADSC : ADC Start, ADATE : ADC Auto Trigger Enable, ADIE : ADC Interrupt Enable,  ADPS : ADC Prescaler  */
-  // free running ADC mode, f = ( 16MHz / prescaler ) / 13 cycles per conversion 
-  ADMUX = _BV(REFS0) | IR_AUDIO; // | _BV(ADLAR); 
-  //  ADCSRA = _BV(ADSC) | _BV(ADEN) | _BV(ADATE) | _BV(ADIE) | _BV(ADPS2) | _BV(ADPS1) //prescaler 64 : 19231 Hz - 300Hz per 64 divisins
-  ADCSRA = _BV(ADSC) | _BV(ADEN) | _BV(ADATE) | _BV(ADIE) | _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0); // prescaler 128 : 9615 Hz - 150 Hz per 64 divisions, better for most music
-  sei();
-  
-  
-  iColor = random(256 * COLOR_SHIFT_SPEED);
 
 }
 
 void hmAudioLoop() {
-  
-    if (audioBufferPosition == FFT_N) {
-      fft_input(capture, bfly_buff);
-      fft_execute(bfly_buff);
-      fft_output(bfly_buff, spectrum);
-      
-      for (int i = 0; i < NUM_BUCKETS; i++) {
-        int16_t val;
-  /*
-        if (i < 2) {
-          val = spectrum[i];
-        } else {
-          val = spectrum[i * 2 - 2] + spectrum[i * 2 - 1] + spectrum[i * 2];
-        }
-  */
-        val = spectrum[i];
-  
-        float sVal = scaleValue(i, val);
-        
-        float multiplier = sVal > sBuffer[i] ? DECAY_UP : -DECAY_DOWN;
-        
-        sBuffer[i] = multiplier * (sVal - sBuffer[i]);
-        sBuffer[i] = sBuffer[i] < 0.0 ? 0 : sBuffer[i] > 255.0 ? 255 : sBuffer[i];
-  
-        /*
-        Serial.print(spectrum[i], DEC);    
-        Serial.print("=>");
-        Serial.print(sVal, DEC);
-        Serial.print("=>");
-        Serial.print(sBuffer[i], DEC);
-        Serial.print(": (");
-        Serial.print(minima[i], DEC);
-        Serial.print("-");
-        Serial.print(maxima[i], DEC);
-        Serial.print("); ");
-        /**/    
-      }  
-      
-      //Serial.println(" ");
-      
-      for (int i = 0; i < NUM_LIGHTS; i++) {
-        uint16_t c = sBuffer[i % NUM_BUCKETS + 1] + sBuffer[0];
-        strip.setPixelColor(i, StripUtils().getWheelColor(c * settings.brightness , (iColor / COLOR_SHIFT_SPEED + c  ) % 256));
-      }
-      
-      iColor++;
-      if (iColor > 255 * COLOR_SHIFT_SPEED) {
-        iColor = 0;
-      }
-      
-      strip.show();
-      audioBufferPosition = 0;
-    }
+   int   i;
+
+   //Filling up input raw data array x[];
+ // 14.6 msec for ADC and 12.4 msec everything else, TOTAL = 27 msec if FFT size = 64 (N=128).
+ // 28.8 msec for ADC and 27.1 msec everything else, TOTAL = 55.9 msec if FFT size = 128 (N).
+
+   ADCSRA = 0x87;
+   // turn on adc, freq = 1/128 , 125 kHz.  
+   //ADMUX = 0x60 | IR_AUDIO;
+   ADMUX = _BV(REFS0) | IR_AUDIO | _BV(ADLAR); 
+   //Bit 5 – ADLAR: ADC Left Adjust Result
+   
+   ADCSRA |= (1<<ADSC);
+   //    while((ADCSRA&(1<<ADIF)) == 0); //Discard first conversion result.
+   while(!(ADCSRA & 0x10));
+
+   for(i=0; i<N; i++ ) {
+     ADCSRA |= (1<<ADSC);
+     //    while((ADCSRA&(1<<ADIF)) == 0);
+     while(!(ADCSRA & 0x10));
+
+     x[i] = ADCL;
+     x[i] += (ADCH << 8);  
+   }  
+
+   ADCSRA = 0x00;
+
+   for (i=0; i<N; i++){
+     x[i] -=  sdvig;
+     if (i & 0x01)
+       fx[(N+i)>>1] = x[i] ;
+     else
+       fx[i>>1] = x[i] ;
+   }  
+
+ //Performing FFT, getting fx[] array, where each element represents
+ //frequency bin with width 65 Hz.
+
+   fix_fftr( fx, log2N );
+
+ // Calculation of the magnitude:
+   for (i=0; i<N/2; i++)
+   {
+     fx[i] = sqrt((long)fx[i] * (long)fx[i] + (long)fx[i+N/2] * (long)fx[i+N/2]);
+   }
+   
+   
+   for (i = 0; i < 18; i ++) {
+     strip.setPixelColor(i, StripUtils().getColor(map(fx[i], minim, maxim, 0, 127), 0, 0));
+   }
+   strip.show();
+
 }
 
 
-
-void clearMinMax() {
-   for (int i = 0; i < NUM_BUCKETS; i++) {
-    minima[i] = 0x80;
-    maxima[i] = 0x81;
-  }
-}
-
-float scaleValue(int i, float val) {
-//  if (minima[i] < maxima[i]) {
-    minima[i] *= (1 + SCALE_DECAY);
-    //minima[i] = max(1, pow(minima[i], (1 + SCALE_DECAY)));
-//  }
-//  if (maxima[i] > minima[i]) {
-     maxima[i] *= (1 - SCALE_DECAY);
-     //maxima[i] = pow(maxima[i], (1 - SCALE_DECAY));
-//  }
-  
-  
-  if (minima[i] > val * 0.9) {
-    minima[i] = val * 0.9;
-  }
-  
-  if (maxima[i] < val * 1.1) {
-    maxima[i] = val * 1.1;        
-  }
-  
-  if (maxima[i] - minima[i] < 40 / i) {
-    minima[i] = max(0, minima[i] - 20 / i);
-    maxima[i] = minima[i] + 40 / i;
-  }
-    
-  return StripUtils().fscale(minima[i], maxima[i], 0, 0xff, val, 0);
-}
-
-
-/*
-// free running ADC fills capture buffer
-ISR(ADC_vect)
-{
-  if (audioBufferPosition >= FFT_N)
-    return;
-
-  capture[audioBufferPosition] = ADC; // + zero;
-  if (capture[audioBufferPosition] == -1 || capture[audioBufferPosition] == 1)
-    capture[audioBufferPosition] = 0;
-
-  audioBufferPosition++;
-}
-*/
 
 void hmAudioShutdown() {
-  
+   ADCSRA = 0x97;
+   ADMUX = 0x00;
+
 }
 
 
 
+
+
+
+
+
+
+
+ //**************************************************************************************************
+
+
+ const prog_int16_t Sinewave[N_WAVE-N_WAVE/4] PROGMEM = {
+   0,    201,    402,    603,    804,   1005,   1206,   1406,
+   1607,   1808,   2009,   2209,   2410,   2610,   2811,   3011,
+   3211,   3411,   3611,   3811,   4011,   4210,   4409,   4608,
+   4807,   5006,   5205,   5403,   5601,   5799,   5997,   6195,
+   6392,   6589,   6786,   6982,   7179,   7375,   7571,   7766,
+   7961,   8156,   8351,   8545,   8739,   8932,   9126,   9319,
+   9511,   9703,   9895,  10087,  10278,  10469,  10659,  10849,
+   11038,  11227,  11416,  11604,  11792,  11980,  12166,  12353,
+   12539,  12724,  12909,  13094,  13278,  13462,  13645,  13827,
+   14009,  14191,  14372,  14552,  14732,  14911,  15090,  15268,
+   15446,  15623,  15799,  15975,  16150,  16325,  16499,  16672,
+   16845,  17017,  17189,  17360,  17530,  17699,  17868,  18036,
+   18204,  18371,  18537,  18702,  18867,  19031,  19194,  19357,
+   19519,  19680,  19840,  20000,  20159,  20317,  20474,  20631,
+   20787,  20942,  21096,  21249,  21402,  21554,  21705,  21855,
+   22004,  22153,  22301,  22448,  22594,  22739,  22883,  23027,
+   23169,  23311,  23452,  23592,  23731,  23869,  24006,  24143,
+   24278,  24413,  24546,  24679,  24811,  24942,  25072,  25201,
+   25329,  25456,  25582,  25707,  25831,  25954,  26077,  26198,
+   26318,  26437,  26556,  26673,  26789,  26905,  27019,  27132,
+   27244,  27355,  27466,  27575,  27683,  27790,  27896,  28001,
+   28105,  28208,  28309,  28410,  28510,  28608,  28706,  28802,
+   28897,  28992,  29085,  29177,  29268,  29358,  29446,  29534,
+   29621,  29706,  29790,  29873,  29955,  30036,  30116,  30195,
+   30272,  30349,  30424,  30498,  30571,  30643,  30713,  30783,
+   30851,  30918,  30984,  31049,  31113,  31175,  31236,  31297,
+   31356,  31413,  31470,  31525,  31580,  31633,  31684,  31735,
+   31785,  31833,  31880,  31926,  31970,  32014,  32056,  32097,
+   32137,  32176,  32213,  32249,  32284,  32318,  32350,  32382,
+   32412,  32441,  32468,  32495,  32520,  32544,  32567,  32588,
+   32609,  32628,  32646,  32662,  32678,  32692,  32705,  32717,
+   32727,  32736,  32744,  32751,  32757,  32761,  32764,  32766,
+   32767,  32766,  32764,  32761,  32757,  32751,  32744,  32736,
+   32727,  32717,  32705,  32692,  32678,  32662,  32646,  32628,
+   32609,  32588,  32567,  32544,  32520,  32495,  32468,  32441,
+   32412,  32382,  32350,  32318,  32284,  32249,  32213,  32176,
+   32137,  32097,  32056,  32014,  31970,  31926,  31880,  31833,
+   31785,  31735,  31684,  31633,  31580,  31525,  31470,  31413,
+   31356,  31297,  31236,  31175,  31113,  31049,  30984,  30918,
+   30851,  30783,  30713,  30643,  30571,  30498,  30424,  30349,
+   30272,  30195,  30116,  30036,  29955,  29873,  29790,  29706,
+   29621,  29534,  29446,  29358,  29268,  29177,  29085,  28992,
+   28897,  28802,  28706,  28608,  28510,  28410,  28309,  28208,
+   28105,  28001,  27896,  27790,  27683,  27575,  27466,  27355,
+   27244,  27132,  27019,  26905,  26789,  26673,  26556,  26437,
+   26318,  26198,  26077,  25954,  25831,  25707,  25582,  25456,
+   25329,  25201,  25072,  24942,  24811,  24679,  24546,  24413,
+   24278,  24143,  24006,  23869,  23731,  23592,  23452,  23311,
+   23169,  23027,  22883,  22739,  22594,  22448,  22301,  22153,
+   22004,  21855,  21705,  21554,  21402,  21249,  21096,  20942,
+   20787,  20631,  20474,  20317,  20159,  20000,  19840,  19680,
+   19519,  19357,  19194,  19031,  18867,  18702,  18537,  18371,
+   18204,  18036,  17868,  17699,  17530,  17360,  17189,  17017,
+   16845,  16672,  16499,  16325,  16150,  15975,  15799,  15623,
+   15446,  15268,  15090,  14911,  14732,  14552,  14372,  14191,
+   14009,  13827,  13645,  13462,  13278,  13094,  12909,  12724,
+   12539,  12353,  12166,  11980,  11792,  11604,  11416,  11227,
+   11038,  10849,  10659,  10469,  10278,  10087,   9895,   9703,
+   9511,   9319,   9126,   8932,   8739,   8545,   8351,   8156,
+   7961,   7766,   7571,   7375,   7179,   6982,   6786,   6589,
+   6392,   6195,   5997,   5799,   5601,   5403,   5205,   5006,
+   4807,   4608,   4409,   4210,   4011,   3811,   3611,   3411,
+   3211,   3011,   2811,   2610,   2410,   2209,   2009,   1808,
+   1607,   1406,   1206,   1005,    804,    603,    402,    201,
+   0,   -201,   -402,   -603,   -804,  -1005,  -1206,  -1406,
+   -1607,  -1808,  -2009,  -2209,  -2410,  -2610,  -2811,  -3011,
+   -3211,  -3411,  -3611,  -3811,  -4011,  -4210,  -4409,  -4608,
+   -4807,  -5006,  -5205,  -5403,  -5601,  -5799,  -5997,  -6195,
+   -6392,  -6589,  -6786,  -6982,  -7179,  -7375,  -7571,  -7766,
+   -7961,  -8156,  -8351,  -8545,  -8739,  -8932,  -9126,  -9319,
+   -9511,  -9703,  -9895, -10087, -10278, -10469, -10659, -10849,
+   -11038, -11227, -11416, -11604, -11792, -11980, -12166, -12353,
+   -12539, -12724, -12909, -13094, -13278, -13462, -13645, -13827,
+   -14009, -14191, -14372, -14552, -14732, -14911, -15090, -15268,
+   -15446, -15623, -15799, -15975, -16150, -16325, -16499, -16672,
+   -16845, -17017, -17189, -17360, -17530, -17699, -17868, -18036,
+   -18204, -18371, -18537, -18702, -18867, -19031, -19194, -19357,
+   -19519, -19680, -19840, -20000, -20159, -20317, -20474, -20631,
+   -20787, -20942, -21096, -21249, -21402, -21554, -21705, -21855,
+   -22004, -22153, -22301, -22448, -22594, -22739, -22883, -23027,
+   -23169, -23311, -23452, -23592, -23731, -23869, -24006, -24143,
+   -24278, -24413, -24546, -24679, -24811, -24942, -25072, -25201,
+   -25329, -25456, -25582, -25707, -25831, -25954, -26077, -26198,
+   -26318, -26437, -26556, -26673, -26789, -26905, -27019, -27132,
+   -27244, -27355, -27466, -27575, -27683, -27790, -27896, -28001,
+   -28105, -28208, -28309, -28410, -28510, -28608, -28706, -28802,
+   -28897, -28992, -29085, -29177, -29268, -29358, -29446, -29534,
+   -29621, -29706, -29790, -29873, -29955, -30036, -30116, -30195,
+   -30272, -30349, -30424, -30498, -30571, -30643, -30713, -30783,
+   -30851, -30918, -30984, -31049, -31113, -31175, -31236, -31297,
+   -31356, -31413, -31470, -31525, -31580, -31633, -31684, -31735,
+   -31785, -31833, -31880, -31926, -31970, -32014, -32056, -32097,
+   -32137, -32176, -32213, -32249, -32284, -32318, -32350, -32382,
+   -32412, -32441, -32468, -32495, -32520, -32544, -32567, -32588,
+   -32609, -32628, -32646, -32662, -32678, -32692, -32705, -32717,
+   -32727, -32736, -32744, -32751, -32757, -32761, -32764, -32766
+ };
+
+ int fix_fft(int fr[], int fi[], int m )
+ {
+   int mr, nn, i, j, l, k, istep, n, scale, shift;
+   int qr, qi, tr, ti, wr, wi;
+
+   n = 1 << m;
+
+   /* max FFT size = N_WAVE */
+   if (n > N_WAVE)
+     return -1;
+
+   mr = 0;
+   nn = n - 1;
+   scale = 0;
+
+   /* decimation in time - re-order data */
+   for (m=1; m<=nn; ++m) {
+     l = n;
+     do {
+       l >>= 1;
+     } 
+     while (mr+l > nn);
+     mr = (mr & (l-1)) + l;
+
+     if (mr <= m)
+       continue;
+     tr = fr[m];
+     fr[m] = fr[mr];
+     fr[mr] = tr;
+     ti = fi[m];
+     fi[m] = fi[mr];
+     fi[mr] = ti;
+   }
+
+   l = 1;
+   k = LOG2_N_WAVE-1;
+   while (l < n) {
+     shift = 1;
+     istep = l << 1;
+     for (m=0; m<l; ++m) {
+       j = m << k;
+       /* 0 <= j < N_WAVE/2 */
+       wr =  pgm_read_word(&Sinewave[j+N_WAVE/4]);
+       wi = -pgm_read_word(&Sinewave[j]);
+
+       wr >>= 1;
+       wi >>= 1;
+
+       for (i=m; i<n; i+=istep) {
+         j = i + l;
+         tr = ((long)wr*(long)fr[j] - (long)wi*(long)fi[j])>>15;
+         ti = ((long)wr*(long)fi[j] + (long)wi*(long)fr[j])>>15;
+         qr = fr[i];
+         qi = fi[i];
+
+         qr >>= 1;
+         qi >>= 1;
+
+         fr[j] = qr - tr;
+         fi[j] = qi - ti;
+         fr[i] = qr + tr;
+         fi[i] = qi + ti;
+       }
+     }
+     --k;
+     l = istep;
+   }
+   return scale;
+ }
+
+ int fix_fftr(int f[], int m )
+ {
+   int i, Nt = 1<<(m-1),  scale = 0;
+   int tt, *fr=f, *fi=&f[Nt];
+
+   scale = fix_fft(fi, fr, m-1 );
+   return scale;
+ }
+ //**************************************************************************************************
